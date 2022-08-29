e441c0295dedd3aa890538aaabc48feeb037944f
Brice <lebowvsky@gmail.com>
2022-08-29 22:56:38 +0200

    🎉✨ init and develop

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,39 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
+import axios from "axios";
 
 export default createStore({
   state: {
+    products: [],
+    productsInBag: [],
   },
-  getters: {
-  },
+  getters: {},
   mutations: {
+    loadProducts(state, products) {
+      state.products = products;
+    },
+    addToBag(state, product) {
+      state.productsInBag = [...state.productsInBag, product];
+      console.log(state.productsInBag);
+    },
+    removeFromBag(state, productId) {
+      if (confirm("Are you sure you want to remove the item from bag ?")) {
+        state.productsInBag = state.productsInBag.filter((elt) => elt.id != productId);
+      }
+    },
   },
   actions: {
+    async loadProducts({ commit }) {
+      const result = await axios.get("https://fakestoreapi.com/products");
+      commit("loadProducts", result.data);
+    },
+
+    addToBag({ commit }, product) {
+      commit("addToBag", product);
+    },
+
+    removeFromBag({ commit }, productId) {
+      commit("removeFromBag", productId);
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
