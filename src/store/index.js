9ded358a64ace5d8e6341a55defabdd0b1eff1b9
Brice <lebowvsky@gmail.com>
2022-08-30 22:46:22 +0200

    ✨ add LocalStorage to keep the Basket

--- src/store/index.js
+++ src/store/index.js
@@ -11,20 +11,31 @@ export default createStore({
     loadProducts(state, products) {
       state.products = products;
     },
+    loadBag(state, products) {
+      state.productsInBag = products;
+    },
     addToBag(state, product) {
       state.productsInBag = [...state.productsInBag, product];
-      console.log(state.productsInBag);
+      localStorage.setItem("productsInBag", JSON.stringify(state.productsInBag));
     },
     removeFromBag(state, productId) {
       if (confirm("Are you sure you want to remove the item from bag ?")) {
         state.productsInBag = state.productsInBag.filter((elt) => elt.id != productId);
+        localStorage.setItem("productsInBag", JSON.stringify([]));
       }
     },
   },
+
   actions: {
     async loadProducts({ commit }) {
       const result = await axios.get("https://fakestoreapi.com/products");
       commit("loadProducts", result.data);
+    },
+
+    async loadBag({ commit }) {
+      if (localStorage.getItem("productsInBag")) {
+        commit("loadBag", JSON.parse(localStorage.getItem("productsInBag")));
+      }
     },
 
     addToBag({ commit }, product) {
